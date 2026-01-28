006e537ccdb471f2c28356df3ad43e51c47c6f75
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2026-01-28 21:57:42 +0100

    Initial

---
+++ ble_ota.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd $( dirname $0 ) && pwd )
+BLE_OTA_REPO_DIR=$SCRIPT_DIR/../ble_ota_app
+BLE_OTA_BUILD_DIR=$BLE_OTA_REPO_DIR/build
+BLE_OTA_WEB_DIR=$BLE_OTA_BUILD_DIR/web
+BLE_OTA_OUT_DIR=$SCRIPT_DIR/apps/ble_ota
+
+cd $BLE_OTA_REPO_DIR
+flutter clean
+flutter build web --release
+
+mkdir -p $BLE_OTA_OUT_DIR
+cp -r $BLE_OTA_WEB_DIR/. $BLE_OTA_OUT_DIR
