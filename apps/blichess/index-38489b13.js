4b37d578bb913c0c7b165d641345d31cc55b2c00
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2026-01-30 21:20:02 +0100

    Disable network

---
+++ apps/blichess/index-38489b13.js
@@ -0,0 +1,1 @@
+import{al as e}from"./main.js";const a=e("Share",{web:()=>import("./web-38018861.js").then((e=>new e.ShareWeb))});export{a as S};
--- apps/blichess/index-c70fdca8.js
+++
@@ -1,1 +0,0 @@
-import{ap as e}from"./main.js";const a=e("Share",{web:()=>import("./web-3df12c3d.js").then((e=>new e.ShareWeb))});export{a as S};
